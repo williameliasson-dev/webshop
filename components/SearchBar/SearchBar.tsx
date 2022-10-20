5062cd17b0291a32621e8a233a9e881a81aeaac6
“Apspelet33” <williamstarwars1@hotmail.com>
2022-10-20 20:02:02 +0200

    search page

--- components/SearchBar/SearchBar.tsx
+++ components/SearchBar/SearchBar.tsx
@@ -1,13 +1,21 @@
+import { Product } from "@prisma/client";
 import React, { useState, useEffect } from "react";
+import styles from "./SearchBar.module.scss";
+import { useRouter } from "next/router";
 
 type Props = {};
 
 const SearchBar = (props: Props) => {
   const [query, setQuery] = useState("");
-  const [results, setResults] = useState({});
+  const [results, setResults] = useState([]);
+  const router = useRouter();
 
   useEffect(() => {
-    fetchResult();
+    if (query.length !== 0) {
+      fetchResult();
+    } else {
+      setResults([]);
+    }
   }, [query]);
 
   async function fetchResult() {
@@ -18,38 +26,58 @@ const SearchBar = (props: Props) => {
       },
       method: "POST",
       body: JSON.stringify({ query: query }),
-    }).then(async (res) => setResults(await res.json()));
+    }).then(async (res) => {
+      const data = await res.json();
+      if ((await data) !== results) {
+        setResults(data);
+      }
+    });
     console.log(results);
   }
 
   return (
-    <div>
-      <div>
-        <label htmlFor="search-input">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            className="feather feather-search"
-          >
-            <circle cx="11" cy="11" r="8"></circle>
-            <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
-          </svg>
-          <input
-            value={query}
-            onChange={(e) => setQuery(e.target.value)}
-            id="search-input"
-            placeholder="Sök..."
-          />
-        </label>
+    <div className={styles.container}>
+      <div className={styles.inner}>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            router.push(`/search?products=${query}`);
+          }}
+        >
+          <label htmlFor="search-input" className={styles.search}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="feather feather-search"
+            >
+              <circle cx="11" cy="11" r="8"></circle>
+              <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
+            </svg>
+            <input
+              value={query}
+              onChange={(e) => setQuery(e.target.value)}
+              id="search-input"
+              placeholder="Sök..."
+            />
+          </label>
+        </form>
+        <div className={styles.results}>
+          {results?.map((result: Product, i) => {
+            return (
+              <div className={styles.result} key={i}>
+                <h3>{result.title}</h3>
+              </div>
+            );
+          })}
+        </div>
       </div>
-      <div>a</div>
     </div>
   );
 };
---
+++ pages/search.tsx
@@ -0,0 +1,65 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import Button from "../components/Button/Button";
+import { PrismaClient, Product } from "@prisma/client";
+import styles from "../styles/Nyheter.module.scss";
+
+type Props = {
+  products: Array<Product>;
+};
+
+const Search = ({ products }: Props) => {
+  const router = useRouter();
+
+  return (
+    <div className={styles.container}>
+      <h1>Visar resultat för: {router.query.products}</h1>
+      <div className={styles.products}>
+        {products.map((product, i) => {
+          return (
+            <div className={styles.product} key={i}>
+              <Link href={""}>
+                <a className={styles.imagewrapper}>
+                  <Image
+                    layout="fill"
+                    alt="product"
+                    src={`${product.imgLink}`}
+                  />
+                </a>
+              </Link>
+              <h2>{product.title}</h2>
+              <p>{product.desc}</p>
+              <p>{product.price}kr</p>
+              <Button variant="btn">Köp</Button>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Search;
+
+export async function getServerSideProps({ query }: any) {
+  const prisma = new PrismaClient();
+  const products = await prisma.product.findMany({
+    take: 10,
+    where: {
+      title: {
+        contains: query.products,
+        mode: "insensitive",
+      },
+    },
+    orderBy: {
+      title: "asc",
+    },
+  });
+  return {
+    props: {
+      products: JSON.parse(JSON.stringify(products)),
+    }, // will be passed to the page component as props
+  };
+}
