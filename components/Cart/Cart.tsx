4c73da3db0e913733c117b2a393a552a9cb820bb
“Apspelet33” <williamstarwars1@hotmail.com>
2022-11-08 17:13:56 +0100

    nav layout

--- components/Cart/Cart.tsx
+++ components/Cart/Cart.tsx
@@ -41,12 +41,12 @@ const Cart = (props: Props) => {
         <div className={styles.cart}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
+            width="20"
+            height="20"
             viewBox="0 0 24 24"
             fill="none"
-            stroke="white"
-            strokeWidth="2"
+            stroke="black"
+            strokeWidth="1.5"
             strokeLinecap="round"
             strokeLinejoin="round"
             className="feather feather-shopping-cart"
--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -3,6 +3,7 @@ import styles from "./Navbar.module.scss";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import SearchBar from "../SearchBar/SearchBar";
+import Button from "../Button/Button";
 import Cart from "../Cart/Cart";
 import { useSession, signIn, signOut } from "next-auth/react";
 
@@ -10,8 +11,41 @@ type Props = {};
 
 const Navbar = (props: Props) => {
   const { data: session }: any = useSession();
+  const [toggleMenu, setToggleMenu] = useState(false);
 
-  return <div className={styles.container}></div>;
+  return (
+    <div className={styles.container}>
+      <div>
+        <div className={styles.navbar}>
+          <h3>WEBSHOP</h3>
+          <div className={styles.actions}>
+            {" "}
+            <Cart />
+            <Button variant="invis" onClick={() => setToggleMenu(!toggleMenu)}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 20"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="feather feather-align-justify"
+              >
+                <line x1="21" y1="10" x2="3" y2="10" />
+                <line x1="21" y1="6" x2="3" y2="6" />
+                <line x1="21" y1="14" x2="3" y2="14" />
+                <line x1="21" y1="18" x2="3" y2="18" />
+              </svg>
+            </Button>
+          </div>
+        </div>
+      </div>
+      {toggleMenu && <div>aaa</div>}
+    </div>
+  );
 };
 
 export default Navbar;
