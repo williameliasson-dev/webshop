8bd998aa28c8c67b06992f629764d85512e7e5c1
“Apspelet33” <williamstarwars1@hotmail.com>
2022-11-09 23:55:10 +0100

    cart visual

--- components/Cart/Cart.tsx
+++ components/Cart/Cart.tsx
@@ -1,66 +1,14 @@
 import React, { useState, useEffect } from "react";
-import Image from "next/image";
-import Link from "next/link";
 import styles from "./Cart.module.scss";
 import { useAppSelector } from "../../store/hooks";
-import Button from "../Button/Button";
 
 type Props = {};
 
 const Cart = (props: Props) => {
-  const cart = useAppSelector((state) => state.cart);
-  const [drawer, setDrawer] = useState(false);
-
   return (
     <div>
-      {drawer && (
-        <div className={styles.drawer}>
-          {cart.products.length === 0 && <h3>Din varukorg är tom...</h3>}
-          {cart.products.map((p, i) => {
-            return (
-              <div className={styles.product} key={i}>
-                <Image
-                  src={p.imgLink}
-                  height="139.5px"
-                  width="120px"
-                  alt="product-img"
-                />
-                <p>{p.title}</p>
-                <p>{p.amount}</p>
-              </div>
-            );
-          })}
-          <Link href="/checkout">
-            <a>
-              <Button>Checkout</Button>
-            </a>
-          </Link>
-        </div>
-      )}
-      <button className={styles["cart-btn"]} onClick={() => setDrawer(!drawer)}>
-        <div className={styles.cart}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="20"
-            height="20"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="black"
-            strokeWidth="1.5"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            className="feather feather-shopping-cart"
-          >
-            <circle cx="9" cy="21" r="1"></circle>
-            <circle cx="20" cy="21" r="1"></circle>
-            <path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path>
-          </svg>
-          {cart.products.length > 0 && (
-            <span>
-              <p>{cart.amount}</p>
-            </span>
-          )}
-        </div>
+      <button className={styles["cart-btn"]}>
+        <div className={styles.cart}></div>
       </button>
     </div>
   );
--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,27 +1,39 @@
 import React, { useState, useEffect, useRef } from "react";
 import styles from "./Navbar.module.scss";
 import Link from "next/link";
+import Image from "next/image";
 import SearchBar from "../SearchBar/SearchBar";
 import Button from "../Button/Button";
 import Cart from "../Cart/Cart";
 import { useSession, signIn, signOut } from "next-auth/react";
+import { useAppSelector } from "../../store/hooks";
 
 type Props = {};
 
 const Navbar = (props: Props) => {
   const { data: session }: any = useSession();
+  const cart = useAppSelector((state) => state.cart);
   const [toggleMenu, setToggleMenu] = useState(false);
+  const [toggleCart, setToggleCart] = useState(false);
   const [searching, setSearching] = useState(false);
   const navref = useRef<HTMLDivElement>(null);
   useEffect(() => {
     if (toggleMenu && navref.current) {
       navref.current.style.borderBottomColor = "gray";
+      if (toggleMenu === true && toggleCart === true) {
+        setToggleCart(false);
+      }
     } else {
       navref.current
         ? (navref.current.style.borderBottomColor = "black")
         : null;
     }
   }, [toggleMenu]);
+  useEffect(() => {
+    if (toggleMenu === true && toggleCart === true) {
+      setToggleMenu(false);
+    }
+  }, [toggleCart]);
 
   return (
     <div className={styles.container}>
@@ -31,8 +43,27 @@ const Navbar = (props: Props) => {
             <h3 onClick={() => setToggleMenu(false)}>WEBSHOP</h3>
           </Link>
           <div className={styles.actions}>
-            {" "}
-            <Cart />
+            <Button variant="invis" onClick={() => setToggleCart(!toggleCart)}>
+              <div className={styles.cartaction}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="20"
+                  height="20"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="black"
+                  strokeWidth="1.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="feather feather-shopping-cart"
+                >
+                  <circle cx="9" cy="21" r="1"></circle>
+                  <circle cx="20" cy="21" r="1"></circle>
+                  <path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path>
+                </svg>
+                {cart.products.length > 0 && <span>{cart.amount}</span>}
+              </div>
+            </Button>
             <Button variant="invis" onClick={() => setToggleMenu(!toggleMenu)}>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -75,6 +106,38 @@ const Navbar = (props: Props) => {
               </nav>
             </div>
           )}
+        </div>
+      )}
+      {toggleCart && (
+        <div className={styles.cart}>
+          <div>
+            {cart?.products?.map((p, i) => {
+              return (
+                <div className={styles.product} key={i}>
+                  <div className={styles["product-row"]}>
+                    <Image
+                      src={p.imgLink}
+                      alt="product"
+                      height={"70px"}
+                      width={"60px"}
+                    />
+                    <span>
+                      <p>{p.title}</p>
+                      {p.amount > 1 && <p>Quantity: {p.amount}</p>}
+                    </span>
+
+                    <span>
+                      <p>Price: {p.price}kr</p>
+                      {p.amount > 1 && <p>Total: {p.price * p.amount}kr</p>}
+                    </span>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+          <Link href={"/checkout"}>
+            <Button>Checkout</Button>
+          </Link>
         </div>
       )}
     </div>
